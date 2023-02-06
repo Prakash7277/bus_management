21945eb64ae82bc1e936409f442f77e7242ad1af
Prakash7277 <pkpatil.366@gmail.com>
2023-02-06 17:37:53 +0530

    MODFIED: NEW PAGE ADEDD

---
+++ New Login Page/log.js
@@ -0,0 +1,11 @@
+var name = document.getElementById("txtname").value;
+function fun(){
+    if(name == prakash){
+        document.getElementById("result").innerHTML="right Name"
+    }
+    else
+    {
+        document.getElementById("result").innerHTML="Wrong Name"
+
+    }
+}
